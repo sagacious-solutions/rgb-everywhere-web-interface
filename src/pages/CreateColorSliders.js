5716bc0f56c237348f161a049dcb136bbda831a0
Matt Macdonald <mksmacdonald@gmail.com>
2022-12-28 11:38:54 -0800

    changes reverted

--- src/pages/CreateColorSliders.js
+++ src/pages/CreateColorSliders.js
@@ -6,8 +6,7 @@ import RgbSlider from "./components/RgbSlider";
 import BasicButton from "./components/BasicButton";
 import "./CreateColorSlider.css";
 import useServerCommunication from "../serverCommunication";
-
-const CHRISTMAS_TREE_URL = process.env.REACT_APP_CHRISTMAS_TREE_URL;
+// import CreateNewColorPopup from "./components/CreateNewColorPopup";
 
 let socket = null;
 
@@ -17,16 +16,22 @@ function sendColorUpdate(color) {
 
 function CreateColorSliders(props) {
     const { postColorRequest } = useServerCommunication();
-    const [rgb, setRgb] = useState([0, 0, 0]);
-    const [socketEnabled, setSocketEnabled] = useState(false);
+    const [state, setState] = useState({
+        rgb: [0, 0, 0],
+        socketEnabled: false,
+        // timeoutHasPassed: true,
+        serverUnavailable: false,
+    });
+    // const [state.rgb, setRgb] = useState([0, 0, 0]);
+    // const [state.socketEnabled, setSocketEnabled] = useState(false);
     const [timeoutHasPassed, setTimeoutHasPassed] = useState(true);
-    const [serverUnavailable, setServerUnavailable] = useState(false);
+    // const [state.serverUnavailable, setServerUnavailable] = useState(false);
     let bttnGrpClass = classNames({ buttonGroup: true });
     let componentClass = classNames({ mainComponent: true });
     let liveButtonClass = classNames({
-        serverUnavailable: serverUnavailable,
+        serverUnavailable: state.serverUnavailable,
     });
-    const rgbString = `${rgb[0]},${rgb[1]},${rgb[2]}`;
+    const rgbString = `${state.rgb[0]},${state.rgb[1]},${state.rgb[2]}`;
 
     const connectToSocket = () => {
         socket = io(`http://${props.currentDevice}:5000/`, {
@@ -34,23 +39,24 @@ function CreateColorSliders(props) {
         });
 
         socket.on("connect_error", () => {
-            setServerUnavailable(true);
+            setState({ ...state, serverUnavailable: true });
+            setState({ ...state, timeoutHasPassed: true });
         });
 
         socket.on("connect", () => {
-            sendColorUpdate(rgb);
-            setServerUnavailable(false);
-            setSocketEnabled(true);
+            sendColorUpdate(state.rgb);
+            setState({ ...state, serverUnavailable: false });
+            setState({ ...state, socketEnabled: true });
         });
         socket.on("disconnect", () => {
-            setSocketEnabled(false);
+            setState({ ...state, socketEnabled: false });
         });
     };
 
     const setColorButton = (
         <BasicButton
-            onClick={() => postColorRequest(rgb, props.currentDevice)}
-            buttonText={`Set Tree to RGB(${rgbString})`}
+            onClick={() => postColorRequest(state.rgb, props.currentDevice)}
+            buttonText={`Set device to RGB(${rgbString})`}
             style={{
                 backgroundColor: `rgb(${rgbString})`,
             }}
@@ -59,13 +65,13 @@ function CreateColorSliders(props) {
     const disconnectTreeButton = (
         <BasicButton
             onClick={() => {
-                setSocketEnabled(false);
+                setState({ ...state, socketEnabled: false });
             }}
             buttonText={"Disconnect from tree."}
             style={{ backgroundColor: `black` }}
         />
     );
-    const setColorOrDisconnectButton = socketEnabled
+    const setColorOrDisconnectButton = state.socketEnabled
         ? disconnectTreeButton
         : setColorButton;
 
@@ -74,22 +80,22 @@ function CreateColorSliders(props) {
         // This is necessary to allow the server time to stop polling and run the routine to
         // change the lights. I feel this isn't necessarily the best strategy as that time
         // could be effected by other variables.
-        let TIMEOUT_MS = 1;
-        if (socketEnabled && timeoutHasPassed) {
-            sendColorUpdate(rgb);
+        let TIMEOUT_MS = 2;
+        if (state.socketEnabled && timeoutHasPassed) {
+            sendColorUpdate(state.rgb);
             setTimeoutHasPassed(false);
             setTimeout(() => {
                 setTimeoutHasPassed(true);
             }, TIMEOUT_MS);
         }
-    }, [rgb]);
+    }, [state.rgb]);
 
-    let buttonBackground = socketEnabled ? `rgb(${rgbString})` : "green";
-    buttonBackground = serverUnavailable ? "red" : buttonBackground;
-    let connectionButtonText = socketEnabled
-        ? `Tree set to RGB(${rgbString})`
+    let buttonBackground = state.socketEnabled ? `rgb(${rgbString})` : "green";
+    buttonBackground = state.serverUnavailable ? "red" : buttonBackground;
+    let connectionButtonText = state.socketEnabled
+        ? `Device set to RGB(${rgbString})`
         : "Start Live Connection";
-    connectionButtonText = serverUnavailable
+    connectionButtonText = state.serverUnavailable
         ? "Server Unavailable"
         : connectionButtonText;
     const connectionButton = (
@@ -107,7 +113,11 @@ function CreateColorSliders(props) {
 
     return (
         <div className={componentClass}>
-            <RgbSlider onChange={setRgb} />
+            <RgbSlider
+                onChange={(val) => {
+                    setState({ ...state, rgb: val });
+                }}
+            />
             <div className={bttnGrpClass}>
                 {setColorOrDisconnectButton}
                 {connectionButton}
