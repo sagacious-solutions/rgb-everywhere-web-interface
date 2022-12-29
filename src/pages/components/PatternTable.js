b4964f05ec94148fd740b848b31d812ec9197b71
Matt Macdonald <mksmacdonald@gmail.com>
2022-12-28 16:48:18 -0800

    Table now renders with pattern colors and modifcation icons

--- src/pages/components/PatternTable.js
+++ src/pages/components/PatternTable.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Avatar } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,25 +7,77 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-
-function createData(pattern, replace, remove) {
-    return { pattern, replace, remove };
+import { HighlightOff, PublishedWithChanges } from "@mui/icons-material";
+// import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import PublishedWithChangesIcon from "@mui/icons-material/PublishedWithChanges";
+/**
+ * Takes a color as an Array or Color object and returns the CSS color string
+ * @param {*} color
+ * @returns a CSS compatible color string
+ */
+function colorToCssRgb(color) {
+    return `rgb(${color[0]},${color[1]},${color[2]})`;
 }
 
-const rows = [
-    createData(
-        [
-            [255, 0, 0],
-            [0, 255, 0],
-        ],
-        null,
-        null
-    ),
-];
+function generatePatternDots(pattern) {
+    let avatars = [];
+
+    pattern.forEach((color, index) => {
+        avatars.push(
+            <Avatar
+                key={`color-circle-${index}`}
+                sx={{
+                    backgroundColor: colorToCssRgb(color),
+                    marginLeft: "2px",
+                    width: "20px",
+                    height: "20px",
+                }}
+                children={""}
+            />
+        );
+    });
+
+    return (
+        <div style={{ display: "flex", flexDirection: "row" }}>{avatars}</div>
+    );
+}
+
+function createTableRows(patterns) {
+    const rows = [];
+    console.log(patterns);
+    patterns.forEach((pattern, i) => {
+        rows.push(
+            <TableRow
+                key={`pattern-${i}`}
+                sx={{
+                    "&:last-child td, &:last-child th": {
+                        border: 0,
+                    },
+                }}
+            >
+                <TableCell component="th" scope="row">
+                    {generatePatternDots(pattern)}
+                </TableCell>
+                <TableCell align="right">
+                    <PublishedWithChanges />
+                </TableCell>
+                <TableCell align="right">
+                    <HighlightOff />
+                </TableCell>
+            </TableRow>
+        );
+    });
+    console.log(rows);
+    return rows;
+}
 
 export default function PatternTable(props) {
+    console.log(props.patterns);
     return (
-        <TableContainer sx={{ width: "60%" }} component={Paper}>
+        <TableContainer
+            sx={{ height: "400px", width: "60%" }}
+            component={Paper}
+        >
             <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
@@ -33,24 +86,7 @@ export default function PatternTable(props) {
                         <TableCell align="right">Delete</TableCell>
                     </TableRow>
                 </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{
-                                "&:last-child td, &:last-child th": {
-                                    border: 0,
-                                },
-                            }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.pattern}
-                            </TableCell>
-                            <TableCell align="right">{row.replace}</TableCell>
-                            <TableCell align="right">{row.remove}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
+                <TableBody>{createTableRows(props.patterns)}</TableBody>
             </Table>
         </TableContainer>
     );
